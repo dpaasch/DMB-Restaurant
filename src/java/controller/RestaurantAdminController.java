8037168998924d2d1cd4b80e69b7bbc47b04d691
tim <tim@timby.gateway.2wire.net>
2013-09-28 21:06:35 -0500

    09/28 21:00 - I GIVE UP!!!!  CANT GET ANYTHING ADMIN RELATED WORKING!!!!

---
+++ src/java/controller/RestaurantAdminController.java
@@ -0,0 +1,102 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.DataAccessException;
+import model.MenuItem;
+import model.MenuService;
+
+/**
+ *
+ * @author tim
+ */
+public class RestaurantAdminController extends HttpServlet {
+    
+    private final static String RESULT_PAGE = "/admin.jsp";
+    
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, DataAccessException {
+
+        MenuService ms = new MenuService();
+        List<MenuItem> menuItems = ms.getAllMenuItems();
+
+        request.setAttribute("menuItems", menuItems);
+
+        // This object lets you forward both the request and response
+        // objects to a destination page
+        RequestDispatcher view = request.getRequestDispatcher(RESULT_PAGE);
+        view.forward(request, response);
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (DataAccessException ex) {
+            Logger.getLogger(RestaurantMenuController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (DataAccessException ex) {
+            Logger.getLogger(RestaurantMenuController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
--- web/index.jsp
+++ web/index.jsp
@@ -18,8 +18,12 @@
         <button onclick="location.href = 'RestaurantMenuController'" name="placeOrder" id="placeOrder">
             <h4>Place Order</h4>
         </button>
+        &Tab;
+        <button onclick="location.href = 'RestaurantAdminController'" name="administerMenu" id="administerMenu">
+            <h4>Administer Menu</h4>
+        </button>
         <br/><br/>
     </center> 
-    <a href="RestaurantAdminController" style="font-style: italic">Administer Blue Bistro</a>
+    <!-- <a href="RestaurantAdminController" style="font-style: italic">Administer Blue Bistro</a> -->
 </body>
 </html>
